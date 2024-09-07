e522b59ab4162f1c6010424eabc45f1fd74bdb41
vipz7747 <vipinkalathiparambil@gmail.com>
2024-09-07 20:44:05 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,12 @@
+function checkOddOrEven(number) {
+    if (number % 2 === 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+checkOddOrEven(1);
+checkOddOrEven(2);
+checkOddOrEven(3);
+checkOddOrEven(4);
